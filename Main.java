7546182d8a6a6ba89a7be9b74549e175caea4ed8
raiki <r.fujiwara.sys22@morijyobi.ac.jp>
2022-11-08 10:12:20 +0900

    間違い訂正

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ public class Main {
 	public static void main(String[] args){
 		double height = 1.69;
 		double weight = 62;
+		double height2 = 1.70;
+		double weight2 = 75;
 		System.out.println(String.format("%.2f",weight/(height*height)));
+		System.out.println(String.format("%.2f",weight2/(height2*height2)));
 	}
 }
