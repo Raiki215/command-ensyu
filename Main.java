6617a8808d7ebcf418682b565cf75c13ab024456
raiki <r.fujiwara.sys22@morijyobi.ac.jp>
2022-11-08 10:07:20 +0900

    コミットしたよ

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args){
+		double height = 1.69;
+		double weight = 62;
+		System.out.println(String.format("%.2f",weight/(height*height)));
+	}
+}
